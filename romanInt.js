69128ac79a3f8332ac729e3bab120357a267cacb
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-10 08:45:24 -0500

    convert roman to int 'wip'

---
+++ romanInt.js
@@ -0,0 +1,29 @@
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+function integerToRoman(num) {
+  var roman = "";
+  var romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  for (var key in romanNumerals) {
+    while (num >= romanNumerals[key]) {
+      roman += key;
+      num -= romanNumerals[key];
+    }
+  }
+  return roman;
+}
+console.log(integerToRoman(3948));
