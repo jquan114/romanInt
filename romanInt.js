14fc26c224b84436cae44919c1e4f74aa3c28a26
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-10 08:45:45 -0500

    Update romanInt.js

--- romanInt.js
+++ romanInt.js
@@ -1,4 +1,4 @@
-Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 function integerToRoman(num) {
   var roman = "";
